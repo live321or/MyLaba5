2db37cdb8050ee5df9b25223ffc785bfefb90e59
live321or <rtyzjixxl28y@mail.ru>
2020-06-05 15:34:54 +0300

    MyLaba5

---
+++ app/src/main/java/com/samoylov/mylaba5/api/model/BreedDTO.java
@@ -0,0 +1,21 @@
+package com.samoylov.mylaba5.api.model;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class BreedDTO {
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getBreed() {
+        return name;
+    }
+}
